3d6f1600aa07fdeaf514888da14011a9bfa1cb37
HEXcube <hexcubed@gmail.com>
2019-03-03 12:49:53 +0530

    Add dynamic cards on Notifications page
    - Disable side drawer on Notifications page

--- src/app/pages/notifications/notifications.page.ts
+++ src/app/pages/notifications/notifications.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-notifications',
@@ -6,10 +7,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notifications.page.scss'],
 })
 export class NotificationsPage implements OnInit {
+  public listCards = [
+    {
+      colorCode: 'green',
+      distance: '300 yds',
+      size: '18"x22"',
+      type: 'Half IPSC'
+    },
+    {
+      colorCode: 'purple',
+      distance: '450 yds',
+      size: '18"x22"',
+      type: 'Half IPSC'
+    },
+    {
+      colorCode: 'white',
+      distance: '600 yds',
+      size: '18"x22"',
+      type: 'Half IPSC'
+    },
+    {
+      colorCode: 'blue',
+      distance: '850 yds',
+      size: '36"x44"',
+      type: 'Full IPSC'
+    }
+  ];
 
-  constructor() { }
+  constructor(
+    public menuCtrl: MenuController
+  ) {}
 
   ngOnInit() {
   }
 
+  /*
+    Disable sidebar
+    https://stackoverflow.com/questions/51610075/disable-menu-on-login-page-ionic-4/51637860#51637860
+  */
+  ionViewWillEnter() {
+    this.menuCtrl.enable(false);
+  }
 }
